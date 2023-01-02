80f4a61f50cc06d6ea1abe538d3ba3cb821fd38e
jaehoon-sim <69947834+jaehoon-sim@users.noreply.github.com>
2023-01-02 09:09:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ html = res.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
+
+index = soup.select("#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div > div > div.flex.w-full.grow.flex-col.py-5px > div > div > div > div > div > div > a > div > div.flex.flex-col > span.font-large2-bold.mb-6pxr.text-el-60.css-1797ph-Text")
 titles = soup.select('#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div > div > div.flex.w-full.grow.flex-col.py-5px > div > div > div > div > div > div > a > div > div.flex.flex-col > span.font-medium2.pb-2pxr.text-el-70.line-clamp-2.css-1797ph-Text')
 thumbnails = soup.select("#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div > div > div.flex.w-full.grow.flex-col.py-5px > div > div > div > div > div > div > a > div > div.relative.overflow-hidden.rounded-3pxr.mr-16pxr.w-80pxr.shrink-0 > div.relative.h-full.w-full.css-v998tn-Image > img")
 genres = soup.select("#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div > div > div.flex.w-full.grow.flex-col.py-5px > div > div > div > div > div > div > a > div > div.flex.flex-col > div > span")
 urls = soup.select("#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div > div > div.flex.w-full.grow.flex-col.py-5px > div > div > div > div > div > div > a")
 
+
+
 book_list = []
-for title, thumbnail, genre, url in zip(titles, thumbnails, genres, urls):
+for idx, title, thumbnail, genre, url in zip(index, titles, thumbnails, genres, urls):
     book_dict = {}
+    
+    book_dict['index'] = idx.text
     book_dict['title'] = title.text  
     book_dict['thumbnail'] = thumbnail['src']
     book_dict['genre'] = genre.text
