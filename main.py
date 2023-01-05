9c1d99a2e255d34dbdcb17e95ac852e9f7a5300e
JaehoonSim <18tomcat@gmail.com>
2023-01-05 22:23:05 +0900

    123

--- main.py
+++ main.py
@@ -2,55 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = 'https://page.kakao.com/landing/ranking/11/117?ranking_type=daily'
+url = 'https://page.kakao.com/_next/data/2.5.0/landing/ranking/11/117.json?ranking_type=daily&pathParams=11&pathParams=117'
 
-req_header_dict = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
-}
+req = requests.get(url)
 
-res = requests.get(url, headers=req_header_dict)
-
-html = res.text
+html = req.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
+books = soup.text
 
-kakaoList = []
-detailList = []
-detail_list = []
-items = soup.select(
-    "#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div > div > div.flex.w-full.grow.flex-col.py-5px > div > div > div > div > div > div > a > div > div.flex.flex-col > span.font-medium2.pb-2pxr.text-el-70.line-clamp-2.css-1797ph-Text")
-
-
-for e, item in enumerate(items, 1):
-    kakao_dict = {}
-    # print(item.text)
-    kakao_dict['title'] = item.text
-    kakao_dict['index'] = e
-    kakaoList.append(kakao_dict)
-
-for idx, book in enumerate(kakaoList, 1):
-    detail_dict = {}
-    book_thumbs = soup.select_one(
-        "#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div > div > div.flex.w-full.grow.flex-col.py-5px > div > div > div > div > div > div > a > div > div.relative.overflow-hidden.rounded-3pxr.mr-16pxr.w-80pxr.shrink-0 > div.relative.h-full.w-full.css-v998tn-Image > img")['src']
-    detail_dict['index'] = book['index']
-    detail_dict['title'] = book['title']
-    detail_dict['thumbnail'] = book_thumbs
-    detailList.append(detail_dict)
-
-for idx, book in enumerate(detailList, 1):
-    detail_final_dict = {}
-    urls = soup.select_one(
-        "#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div > div > div > div.flex.w-full.grow.flex-col.py-5px > div > div > div > div > div > div > a").get('href')
-    detail_final_dict['index'] = book['index']
-    detail_final_dict['title'] = book['title']
-    detail_final_dict['thumbnail'] = book['thumbnail']
-    detail_final_dict['url'] = "https://page.kakao.com"+urls
-    detail_list.append(detail_final_dict)
-
-
-# print(detail_list)
-
+a = json.loads(books)
+rank = 1
+book_list = []
+book_list.append(a)
+book_list2 = []
 
 with open('kakao.json', 'w', encoding='utf-8') as file:
-    json.dump(detail_list, file, ensure_ascii=False, indent='\t')
+    json.dump(book_list, file, ensure_ascii=False, indent="\t")
+
+with open('kakao.json', 'rt', encoding='UTF8') as f:
+    data = json.load(f)
+
+my_list = data[0]["pageProps"]["initialState"]["json"]["pagewebLayout"]["entities"]["items"]
+
+for list_item in my_list:
+    a = my_list[list_item]
+    book_list2.append(a)
+
+print(book_list2)
+# https://page.kakao.com/content/48526879
+
+with open('kakao.json', 'w', encoding='utf-8') as file:
+    json.dump(book_list2, file, ensure_ascii=False, indent="\t")
